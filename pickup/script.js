98fb111fd4a404b8c01e3a917e4d5fbae1049fe3
Imran Raja <imranraja85@gmail.com>
2024-08-24 10:03:48 -0400

    modify alert msg

--- pickup/script.js
+++ pickup/script.js
@@ -27,17 +27,8 @@ domReady(function () {
 });
 
     function copyLink() {
-
-       // Get the text field
        var copyText = document.getElementById("result");
-     
-       // Select the text field
-       //copyText.select();
-       //copyText.setSelectionRange(0, 99999); // For mobile devices
-     
-        // Copy the text inside the text field
        navigator.clipboard.writeText(copyText.innerText);
      
-       // Alert the copied text
-       alert("Copied the text: " + copyText.innerText);
+       alert("Copied the link to your clipboard");
     } 
