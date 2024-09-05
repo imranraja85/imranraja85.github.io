305e662ba7f6470d609dec3cfd5ef110be9506f0
Imran Raja <imranraja85@gmail.com>
2024-09-04 23:32:42 -0400

    readd step

--- pickup/script.js
+++ pickup/script.js
@@ -2,19 +2,22 @@
 
 // If found you qr code
 function onScanSuccess(decodeText, decodeResult) {
-  result = decodeText.replace("viewform?", "formResponse?&submit=Submit&");
-  document.getElementById("result").innerHTML = result;
-  alert("scan complete, go to step 2");
+    result = decodeText.replace("viewform?","formResponse?&submit=Submit&")
+    document.getElementById("result").innerHTML = result;
+    alert('scan complete, go to step 3')
+}
+
+function initScanner() {
+  let htmlscanner = new Html5Qrcode("my-qr-reader");
+
+  config = {};
+
+  htmlscanner.start({ facingMode: "environment" }, config, onScanSuccess);
 }
 
 function copyLink() {
-  var copyText = document.getElementById("result");
-  navigator.clipboard.writeText(copyText.innerText);
-
-  alert("Copied the link to your clipboard, go save it somewhere");
-}
-
-
-let htmlscanner = new Html5Qrcode("my-qr-reader");
-config = {};
-htmlscanner.start({ facingMode: "environment" }, config, onScanSuccess);
+   var copyText = document.getElementById("result");
+   navigator.clipboard.writeText(copyText.innerText);
+ 
+   alert("Copied the link to your clipboard");
+} 
