00be9c28525a7dfad406bba82f25b9801159a10c
Imran Raja <imranraja85@gmail.com>
2025-08-24 16:10:56 -0400

    replace space with %20

--- pickup/script.js
+++ pickup/script.js
@@ -3,6 +3,7 @@
 // If found you qr code
 function onScanSuccess(decodeText, decodeResult) {
     result = decodeText.replace("viewform?","formResponse?&submit=Submit&")
+        .replace(" ","%20")
     document.getElementById("result").innerHTML = result;
     alert('scan complete, go to step 3')
 }
